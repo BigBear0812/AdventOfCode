0378e95eeb8b44e617b2da52c2f1294baf1d04c0
Matthew Miller <millerm784512@gmail.com>
2023-04-08 16:35:32 -0400

    Day 09 Puzzle Solution 2017

---
+++ 2017/Day09.js
@@ -0,0 +1,61 @@
+// Puzzle for Day 9: https://adventofcode.com/2017/day/9
+
+export const run = (fileContents) => {
+  // Split the stream into characters to be evaluated one at a time
+  let stream = fileContents[0].split('');
+
+  // Keep track of several things at once
+  // How many groups deep are we
+  let groupsDeep = 0;
+  // The total score
+  let score = 0;
+  // How many garbage charcacters are there
+  let grabageChars = 0;
+  // The current index
+  let index = 0;
+  // Is this part of a garbage sequence
+  let inGarbage = false;
+
+  // Continue processing the stream while the index is still valid
+  while(index < stream.length){
+    // Get the current character
+    let current = stream[index];
+    // If a ! then skip the next character regardless of being in garbage or not
+    if(current === '!'){
+      index++;
+    }
+    else{
+      // If not in garbage 
+      if(!inGarbage){
+        // Add one to groupsDeep if at an opening brace
+        if(current === '{'){
+          groupsDeep++;
+        }
+        // Update the score then subtract one from groupsDeep if at a closing brace
+        else if(current === '}'){
+          score += groupsDeep;
+          groupsDeep--;
+        }
+        // Set inGarbage to true if finding < while not already in garbage
+        else if(current === '<'){
+          inGarbage = true;
+        }
+      }
+      // If in garbage
+      else{
+        // Set inGarnbage toi false if  > is found while in garbage
+        if(current === '>'){
+          inGarbage = false;
+        }
+        // Else this is just a random garbage character so add one to garbage characters
+        else{
+          grabageChars++;
+        }
+      }
+    }
+    // Add one to the index to advance through the stream.
+    index++;
+  }
+
+  return {part1: score, part2: grabageChars};
+}
