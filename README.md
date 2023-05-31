76a57599f609940f05786c1501aa6ff9ec6d8b8d
Matthew Miller <millerm784512@gmail.com>
2023-05-30 22:08:24 -0400

    Update soltion runner and readme to include the abiloity to time the solution. Cleanup readme documentation.

--- README.md
+++ README.md
@@ -4,11 +4,21 @@ This is my work for [Advent of Code](https://adventofcode.com/)
 
 ## Run a Solution
 
-This command will run a specific solution. YEAR is the event year and DAY is the 2 digit day to be run. 
+This command will run a specific solution.
 
 ```CMD
-npm run day {YEAR} {DAY}
+npm run day <yyyy> <dd> [time]
 ```
+
+### Description
+
+**yyyy**: The 4 digit year of the solution being run.
+
+**dd**: The 2 digit day of the solution being run.
+
+### Options
+
+**time**: Computes the execution time of the solution and displays it after the results are displayed
 
 ## Setup
 
@@ -22,5 +32,6 @@ After cloning the repo run these commands to setup the submodule
 git submodule init
 git submodule update
 ```
+## Input files
 
-
+The solution runner expects that all input files will be in the location `./AOCPuzzlesInputs/{yyyy}/Day{dd}_input.txt`. In this template `{yyyy}` is the 4 digit year and `{dd}` is the 2 digit day.
--- solutionRunner.js
+++ solutionRunner.js
@@ -2,7 +2,7 @@ import process from "node:process";
 import { open } from "node:fs/promises";
 
 // Check that the right number of arguments are present in the command
-if (process.argv.length !== 4){
+if (process.argv.length < 4){
   console.log('Please specify year and day.');
   process.exit(1);
 }
@@ -10,6 +10,7 @@ if (process.argv.length !== 4){
 // Get the file name from the argv values for year and day
 const year = process.argv[2];
 const day = process.argv[3];
+const showTime = process.argv[4];
 // Input file name
 const filename = `./AOCPuzzlesInputs/${year}/Day${day}_input.txt`;
 // Solutions module
@@ -29,11 +30,19 @@ open(filename)
 .then(fileContents => {
   // Import the specified module and run the solution that has been specified
   import(module).then(mod => {
+    let startTime;
+    let totalTime;
+    if(showTime === "time")
+      startTime = Date.now();
     let result = mod.run(fileContents);
-
+    
+    if(startTime)
+      totalTime = Date.now() - startTime;
     if(result.part1 !== null && result.part1 !== undefined)
       console.log("Part 1:", result.part1);
     if(result.part2 !== null && result.part2 !== undefined)
       console.log("Part 2:", result.part2);
+    if(totalTime)
+      console.log("Execution Time:", totalTime / 1000, "s")
   });
 });
